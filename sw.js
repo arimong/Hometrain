5dfa555de9531d1c837879ab788059ea299656a6
arimong <adwjn72@gmail.com>
2026-06-30 10:45:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ht55-v4';
+const CACHE = 'ht55-v6';
 const FILES = ['./index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', e => {
